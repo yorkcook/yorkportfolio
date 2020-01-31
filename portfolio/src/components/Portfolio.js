08d54ed9de902c537d89197c6b99aaaa9e3d8439
York Cook <yorkcook@Johnny-Castle.local>
2020-01-31 13:13:24 -0500

    WIP: cards formatted

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -3,18 +3,24 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
 
 const Portfolio = () => {
   return (
     <div>
       <div class="container ">
-        <div class="row">
+        <div class="row mt-5">
           <div class="col">
             {" "}
             <Card>
               <Card.Img variant="top" src="holder.js/100px160" />
               <Card.Body>
-                <Card.Title>Labby</Card.Title>
+                <Nav.Link
+                  href="https://lambda-labby.netlify.com/"
+                  target="_blank"
+                >
+                  <Card.Title>Labby</Card.Title>
+                </Nav.Link>
                 <Card.Text>
                   This is a wider card with supporting text below as a natural
                   lead-in to additional content. This content is a little bit
@@ -22,7 +28,6 @@ const Portfolio = () => {
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <Button variant="primary">Website</Button>
                 <Button variant="primary">GitHub</Button>
               </Card.Footer>
             </Card>
@@ -40,48 +45,45 @@ const Portfolio = () => {
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <Button variant="primary">Website</Button>
                 <Button variant="primary">GitHub</Button>
               </Card.Footer>
             </Card>
           </div>
-          <div class="row mt-5">
-            <div class="col">
-              {" "}
-              <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                  <Card.Title>We Serve Soup</Card.Title>
-                  <Card.Text>
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <Button variant="primary">Website</Button>
-                  <Button variant="primary">GitHub</Button>
-                </Card.Footer>
-              </Card>
-            </div>
-            <div class="col">
-              {" "}
-              <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                  <Card.Title>Water My Plants</Card.Title>
-                  <Card.Text>
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <Button variant="primary">Website</Button>
-                  <Button variant="primary">GitHub</Button>
-                </Card.Footer>
-              </Card>
-            </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col">
+            {" "}
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>We Serve Soup</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button variant="primary">GitHub</Button>
+              </Card.Footer>
+            </Card>
+          </div>
+          <div class="col">
+            {" "}
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Water My Plants</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button variant="primary">GitHub</Button>
+              </Card.Footer>
+            </Card>
           </div>
         </div>
       </div>
