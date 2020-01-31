19d0dec3ecaf7e621c49eab6484e9df44628fb64
York Cook <yorkcook@Johnny-Castle.local>
2020-01-31 13:26:47 -0500

    WIP: netlify fix try 4

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -14,7 +14,7 @@ const NavBar = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/portfolio">
+              <Nav.Link href="#/portfolio">
                 <Button variant="outline-success">Portfolio</Button>
               </Nav.Link>
               <Nav.Link href="/courses">
