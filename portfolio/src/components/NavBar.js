4a587287e83fa9acb3818394a52a522585168c6c
York Cook <yorkcook@Johnny-Castle.lan1>
2020-01-31 19:27:41 -0500

    WIP: cards formatted

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -5,32 +5,35 @@ import Button from "react-bootstrap/Button";
 
 import { Link } from "react-router-dom";
 
-// import "./NavBar.css";
-
 const NavBar = () => {
   return (
-    <div>
-      <div class="mx-auto" style={{ margin: "150px" }}>
-        <Navbar bg="light" expand="lg" fixed="top">
-          <Navbar.Brand href="/">York Cook</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+    <div class="d-flex nav justify-content-around">
+      <Navbar expand="lg" sticky="top">
+        {/* <Navbar.Brand class="border border-primary" href="/">
+          York Cook
+        </Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <div>
             <Nav className="mr-auto">
+              <Nav.Link href="/">
+                <Button variant="outline-success">Home</Button>
+              </Nav.Link>
               <Nav.Link>
                 <Link to="/portfolio">
                   <Button variant="outline-success">Portfolio</Button>
                 </Link>
               </Nav.Link>
               <Nav.Link href="/courses">
-                <Button variant="outline-success">Courses</Button>
+                <Button variant="outline-success">Continuing Education</Button>
               </Nav.Link>
               {/* <Nav.Link href="/contact">
               <Button variant="outline-success">Contact</Button>
             </Nav.Link> */}
             </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
+          </div>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 };
--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -5,15 +5,26 @@ import CardDeck from "react-bootstrap/CardDeck";
 import Button from "react-bootstrap/Button";
 import Nav from "react-bootstrap/Nav";
 
+import Picture from "./image.png";
+
 const Portfolio = () => {
   return (
     <div>
-      <div class="container ">
+      <div
+        class="container"
+        style={{ marginBottom: "25px", marginTop: "50px" }}
+      >
         <div class="row mt-5">
           <div class="col">
             {" "}
             <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
+              <div class="card">
+                <img
+                  src={Picture}
+                  class="img-fluid"
+                  alt="Responsive image"
+                ></img>
+              </div>
               <Card.Body>
                 <Nav.Link
                   href="https://lambda-labby.netlify.com/"
@@ -28,16 +39,32 @@ const Portfolio = () => {
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <Button variant="primary">GitHub</Button>
+                <Button
+                  variant="primary"
+                  href="https://github.com/yorkcook/be-recipe-vault"
+                  target="_blank"
+                >
+                  GitHub
+                </Button>
               </Card.Footer>
             </Card>
           </div>
           <div class="col">
-            {" "}
             <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
+              <div class="card">
+                <img
+                  src={Picture}
+                  class="img-fluid"
+                  alt="Responsive image"
+                ></img>
+              </div>
               <Card.Body>
-                <Card.Title>Recipe Vault</Card.Title>
+                <Nav.Link
+                  href="https://recipe-vault.netlify.com/"
+                  target="_blank"
+                >
+                  <Card.Title>Recipe Vault</Card.Title>
+                </Nav.Link>
                 <Card.Text>
                   This is a wider card with supporting text below as a natural
                   lead-in to additional content. This content is a little bit
@@ -45,7 +72,13 @@ const Portfolio = () => {
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <Button variant="primary">GitHub</Button>
+                <Button
+                  variant="primary"
+                  href="https://github.com/yorkcook/be-recipe-vault"
+                  target="_blank"
+                >
+                  GitHub
+                </Button>
               </Card.Footer>
             </Card>
           </div>
@@ -54,9 +87,20 @@ const Portfolio = () => {
           <div class="col">
             {" "}
             <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
+              <div class="card">
+                <img
+                  src={Picture}
+                  class="img-fluid"
+                  alt="Responsive image"
+                ></img>
+              </div>
               <Card.Body>
-                <Card.Title>We Serve Soup</Card.Title>
+                <Nav.Link
+                  href="https://we-serve-soup.netlify.com/"
+                  target="_blank"
+                >
+                  <Card.Title>We Serve Soup</Card.Title>
+                </Nav.Link>
                 <Card.Text>
                   This is a wider card with supporting text below as a natural
                   lead-in to additional content. This content is a little bit
@@ -64,16 +108,33 @@ const Portfolio = () => {
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <Button variant="primary">GitHub</Button>
+                <Button
+                  variant="primary"
+                  href="https://github.com/yorkcook/FE"
+                  target="_blank"
+                >
+                  GitHub
+                </Button>
               </Card.Footer>
             </Card>
           </div>
           <div class="col">
             {" "}
             <Card>
-              <Card.Img variant="top" src="holder.js/100px160" />
+              <div class="card">
+                <img
+                  src={Picture}
+                  class="img-fluid"
+                  alt="Responsive image"
+                ></img>
+              </div>
               <Card.Body>
-                <Card.Title>Water My Plants</Card.Title>
+                <Nav.Link
+                  href="https://watermyplants-html.netlify.com/"
+                  target="_blank"
+                >
+                  <Card.Title>Water My Plants</Card.Title>
+                </Nav.Link>
                 <Card.Text>
                   This is a wider card with supporting text below as a natural
                   lead-in to additional content. This content is a little bit
@@ -81,7 +142,13 @@ const Portfolio = () => {
                 </Card.Text>
               </Card.Body>
               <Card.Footer>
-                <Button variant="primary">GitHub</Button>
+                <Button
+                  variant="primary"
+                  href="https://github.com/yorkcook/WEB-UI-III"
+                  target="_blank"
+                >
+                  GitHub
+                </Button>
               </Card.Footer>
             </Card>
           </div>
